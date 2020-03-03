a34528b69dc09e72488cce94980b23ab1c06a9ff
benign-fork-5056 <gareeeliu@gmail.com>
2020-03-03 23:00:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -59,7 +59,7 @@ def apply_coupons(cart, coupons)
         }
         cart << cart_item_with_coupon
         cart_item[:count] -= coupons[counter][:num]
-      end  
+      end
     end
     counter += 1
   end
